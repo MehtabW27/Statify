d6c90802dc56d9c6ef57d0d70cc68edca8339b89
Mehtab Waraich <mehtab.w27@gmail.com>
2022-07-09 15:46:32 -0400

    added navbar

---
+++ statify/src/components/navBar/navBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './navBar.css';
+
+
+
+function NavBar() {
+    return (
+        <nav className="relative container mx-auto p-6">hello
+            <div className="flex items-center justify-between">
+                <div className="pt-2">
+                </div>
+            </div>
+        </nav>
+    );
+  }
+  
+  export default NavBar;
--- statify/tailwind.config.js
+++ statify/tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '968px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        'pearmint': '#4b917d',
+        'brightpink': '#f037a5',
+        'weirdgreen': '#cdf564',
+        'white': '#ffffff',
+        'black': '#000000',
+        'spotifygreen': '#1DB954',
+        'spotifywhite': '#FFFFFF',
+        'spotifyblack': '#191414'
+      }
+    },
   },
   plugins: [],
 }
